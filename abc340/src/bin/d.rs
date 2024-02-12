d73f8993cdb9242a3a5ae828c3035bf05a353020
daimaruonline <dogihara@gmail.com>
2024-02-12 18:40:31 +0900

    abc340/d

--- abc340/src/bin/d.rs
+++ abc340/src/bin/d.rs
@@ -4,23 +4,54 @@ use proconio::input;
 fn main() {
     input!{
         N: usize,
-        ABX: [[usize; 3]; N-1],
     }
-    let mut keisan: Vec<usize> = vec![9999999999; N+1];
-    keisan[1] = 0;
-    for (i, abx) in ABX.iter().enumerate() {
-        let stage = i+1;
-        // println!("stage:{}", stage);
-        if keisan[stage+1] > (keisan[stage] + abx[0]) {
-            keisan[stage+1] = keisan[stage] + abx[0];
+    let mut edge: Vec<Vec<(usize, usize)>> = vec![Vec::new(); N];
+    for i in 0..(N-1) {
+        input!{
+            A: usize,
+            B: usize,
+            X: usize,
+        }
+        edge[i].push((i+1, A));
+        edge[i].push((X-1, B));
+    }
+
+    let mut d = vec![usize::max_value(); N];
+    d[0] = 0;
+    let mut p: Vec<isize> = vec![-1; N];
+    let mut ok: Vec<isize> = vec![-1; N];
+    let mut min_node = 0;
+    loop {
+        let mut min_cost = usize::max_value();
+        for i in 0..N {
+            if ok[i] != 1 && d[i] < min_cost {
+                min_cost = d[i];
+                min_node = i;
+            }
         }
 
-        if keisan[abx[2]] > (keisan[stage] + abx[1]) {
-            keisan[abx[2]] = keisan[stage] + abx[1];
+        if min_cost == usize::max_value() {
+            break;
         }
-        for (i, keisan) in keisan.iter().enumerate() {
-            println!("{} {}", i, keisan);
+        ok[min_node] = 1;
+
+        for &e in &edge[min_node] {
+            let v = e.0;
+            if ok[v] != 1 {
+                if d[min_node] + e.1 < d[v] {
+                    d[v] = d[min_node] + e.1;
+                    p[v] = min_node as isize;
+                    ok[v] = 0;
+                }
+            }
         }
+
+        // println!("# loop:{}", count);
+        // for i in 0..N {
+        //     println!(" edge:{} d:{} p:{} ok:{}", i, d[i], p[i], ok[i]);
+        // }
+        // count++;
     }
-    println!("{}", keisan[N]);
+
+    println!("{}", d[N-1]);
 }
